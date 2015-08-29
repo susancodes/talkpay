2c8dc4a09b814c15dcc1ed34aa16d5d6f80c9138
Susan Chin <Susan@Susans-MacBook-Pro.local>
2015-08-28 20:09:21 -0700

    added javascript file

---
+++ talkpay.js
@@ -0,0 +1,17 @@
+
+// this is for the google charts
+google.load('visualization', '1', {packages: ['corechart']});
+google.setOnLoadCallback(initialize);
+
+
+function initialize() {
+	// add event listener here after google chart initializes
+}
+
+
+function drawChart(chartData) {
+	var salaryList = chartData
+
+	var data = new google.visualization.DataTable();
+
+}
