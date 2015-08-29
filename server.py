064d398b2d32412164f957fbdf9009cb2ed436b4
Lauren <laurenortencio@gmail.com>
2015-08-28 21:58:07 -0700

    added twilio credentials

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db
+from jinja2 import StrictUndefined
+from twilio import twiml
+from twilio.rest import TwilioRestClient
 
 import os
 
 app = Flask(__name__)
 
 app.secret_key = "developHER"
+app.jinja_env.undefined = StrictUndefined
+
+
+##############################################################################
+# **** Twilio **** 
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = TwilioRestClient(account_sid, auth_token)
+to_number = os.environ['TWILIO_TO_NUMBER']
+TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
+
+##############################################################################
 
 
 
